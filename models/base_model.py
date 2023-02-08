ed57fd10fa26f6ada5746a980236908adbed6dc1
Aso Orji <42071241+asoorji@users.noreply.github.com>
2023-02-08 18:27:22 +0000

    Object storage and code refactorization

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """
@@ -12,14 +13,14 @@ class BaseModel:
             common attributes/methods for other classes
     """
     def __init__(self, *args, **kwargs):
-          """
+        """
         The class constructor
         Initializes instance attributes
         Args:
             - *args: list of arguments
             - **kwargs: dict of key-values arguments
         """
-        
+
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
@@ -34,7 +35,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-
+            storage.new(self)
 
     def __str__(self):
         """Returns official string representation of the class"""
@@ -46,9 +47,13 @@ class BaseModel:
         """updates the public instance attribute updated_at"""
 
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        self.__dict__["__class__"] = self.__class__.__name__
-        self.__dict__["created_at"] = self.created_at.isoformat()
-        self.__dict__["updated_at"] = self.updated_at.isoformat()
-        return self.__dict__
+        """returns a dictionary containing all keys-values of __dict__"""
+
+        class_dict = self.__dict__.copy()
+        class_dict["__class__"] = type(self).__name__
+        class_dict["created_at"] = class_dict["created_at"].isoformat()
+        class_dict["updated_at"] = class_dict["updated_at"].isoformat()
+        return class_dict
