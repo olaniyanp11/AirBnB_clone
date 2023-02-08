297eb791fdb9c0520b6a83e5eb86f1496a725d86
Aso Orji <42071241+asoorji@users.noreply.github.com>
2023-02-08 18:23:34 +0000

    Recreating an instance with a dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -6,21 +6,45 @@
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
     """
         a class BaseModel that defines all
             common attributes/methods for other classes
     """
-    def __init__(self):
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+          """
+        The class constructor
+        Initializes instance attributes
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+        
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
 
     def __str__(self):
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        """Returns official string representation of the class"""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
+        """updates the public instance attribute updated_at"""
+
         self.updated_at = datetime.now()
 
     def to_dict(self):
